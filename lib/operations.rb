1d996a9fd6c2a84e88c54145a799b8210c677092
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-07 00:09:15 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,11 @@
 def unsafe?(speed)
-	speed > 60 || speed < 40 ? true : false
+	if speed > 60 || speed < 40
+		true
+	else false
 end
 
 
 
 def not_safe?(speed)
-
+	speed > 60 || speed < 40 ? true : fals
 end
